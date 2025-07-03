68177b857d15cf3c835d4eee715142d63ef89d68
Andrew Peebles <aqpeeb@gmail.com>
2025-07-03 09:46:16 -0700

    remove dist from rc

--- packages/core/dist/fetch.d.ts
+++
@@ -1,4 +0,0 @@
-import { type ClientOptions } from 'openapi-fetch';
-import type { paths } from './schema.gen.ts';
-export declare function createClient(options: ClientOptions): import("openapi-fetch").Client<paths, `${string}/${string}`>;
-export type Client = ReturnType<typeof createClient>;
--- packages/core/dist/schema.d.ts
+++
@@ -1,6 +0,0 @@
-import { paths } from './schema.gen.ts';
-type EventPaths = {
-    [K in keyof paths as K extends `/events/${string}` ? K : never]: paths[K];
-};
-export type { EventPaths };
-export type EventPathKeys = keyof EventPaths;
